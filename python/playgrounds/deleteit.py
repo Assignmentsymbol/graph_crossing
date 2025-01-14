7db254ae660b27b10c5427e748c93ffc90b8a385
Xiaosheng Tan <go68@mytum.de>
2025-01-14 09:49:12 +0100

    fix interesection counter

---
+++ python/playgrounds/deleteit.py
@@ -0,0 +1,19 @@
+r='r=%r;print r%%r';print(r%r)
+# b;a here i think. a is an instruction says"smartly prints something 2 times", b is the description of a adds something else"
+# here isnt real corresponding to the <ab> explicitly i think, a prints stuff not readable which is instruction of b and is
+# not seen for humans and b gets the final string printed.
+# instruction:print 2*self (in a good way
+
+
+print('----------------')
+print("hello worl''d")
+
+r = "This is %rr%%r"
+t="3"
+print(r % t)
+# Declaring multiple string values
+mkr1 = "developers"
+mkr2 = "JavaTpoint"
+mkr3 = "3"
+# Appending multiple string values inside a single string
+print("Hello Python %s, Welcome to the %s! We hope you are having a good %s." % (mkr1, mkr2, mkr3))
