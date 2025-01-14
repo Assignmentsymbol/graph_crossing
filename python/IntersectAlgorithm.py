3934db7b944c8584675cf4166be74b361d414a6b
Xiaosheng Tan <go68@mytum.de>
2025-01-14 10:19:22 +0100

    w

--- python/IntersectAlgorithm.py
+++ python/IntersectAlgorithm.py
@@ -91,6 +91,43 @@ def doIntersect(p1, q1, p2, q2):
     # If none of the cases
     return False
 
+def doIntersect_Fixed(p1, q1, p2, q2):
+    if len((p1, q1, p2, q2)) != len(set((p1, q1, p2, q2))):
+        return False
+    # Find the 4 orientations required for
+    # the general and special cases
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+
+    # Special Cases
+
+    # p1 , q1 and p2 are collinear and p2 lies on segment p1q1
+    if ((o1 == 0) and onSegment(p1, p2, q1)):
+        return 1
+
+    # p1 , q1 and q2 are collinear and q2 lies on segment p1q1
+    if ((o2 == 0) and onSegment(p1, q2, q1)):
+        return 1
+
+    # p2 , q2 and p1 are collinear and p1 lies on segment p2q2
+    if ((o3 == 0) and onSegment(p2, p1, q2)):
+        return 1
+
+    # p2 , q2 and q1 are collinear and q1 lies on segment p2q2
+    if ((o4 == 0) and onSegment(p2, q1, q2)):
+        return 1
+
+        # General case
+    if ((o1 != o2) and (o3 != o4)):
+        return 2
+
+    # If none of the cases
+    return False
+
+
+
 
 
 
